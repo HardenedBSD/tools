ea97d06e5d0c0c1bc991dced24df3aec18b2e03a
HardenedBSD Jenkins <robot@hardenedbsd.org>
2015-06-08 18:03:01 -0400

    added jenkins/nyi-01/001-build-10.zsh
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

---
+++ jenkins/nyi-01/001-build-10.zsh
@@ -0,0 +1,52 @@
+#!/usr/local/bin/zsh
+
+njobs=16
+objdir="/jenkins/objdir/${JOB_NAME}"
+
+target="SHOULD_FAIL"
+targetdir=""
+kernel="HARDENEDBSD"
+
+export __MAKE_CONF="/dev/null"
+export __SRC_CONF="/dev/null"
+export MAKE_CONF="/dev/null"
+export SRC_CONF="/dev/null"
+
+while getopts 'j:t:' o; do
+    case "${o}" in
+        t)
+            if [ ! "${OPTARG}" = "${target}" ]; then
+                case "${OPTARG}" in
+		    amd64)
+                        target="TARGET=amd64 TARGET_ARCH=amd64"
+                        targetdir="amd64"
+                        ;;
+                    i386)
+                        target="TARGET=i386 TARGET_ARCH=i386"
+                        targetdir="i386"
+                        ;;
+                    beaglebone)
+                        target="TARGET=arm TARGET_ARCH=armv6"
+                        targetdir="beaglebone"
+                        kernel="BEAGLEBONE-HARDENEDBSD"
+                        ;;
+                    upstream-amd64)
+                        target="TARGET=amd64 TARGET_ARCH=amd64"
+                        targetdir="amd64"
+                        kernel="GENERIC"
+                        ;;
+                    defaut)
+                        echo "Invalid target!"
+                        exit 1
+                        ;;
+                esac
+            fi
+            ;;
+        j)
+            jobs=${OPTARG}
+            ;;
+    esac
+done
+
+sudo make -j${njobs} buildworld ${target}
+sudo make -j${njobs} buildkernel KERNCONF=${kernel} ${target}
