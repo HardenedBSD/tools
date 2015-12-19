37932616bb9174dd0177c6bcc39fe7e177c0550d
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2015-12-19 18:22:52 +0100

    HBSD: added map32bit related tests
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

---
+++ tests/hbsd-map32bit/test-map32bit-deny1.sh
@@ -0,0 +1,61 @@
+#!/bin/tcsh
+
+set TEST_NAME = "test-map32bit-deny1"
+set TEST_DIR = "/tmp/pax-tests/${USER}/map32bit/"
+
+echo "${TEST_NAME}"
+
+test -d ${TEST_DIR} || mkdir -p ${TEST_DIR}
+cd ${TEST_DIR}
+
+cat > ${TEST_NAME}.c<<__EOF
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+
+#include <sys/types.h>
+#include <sys/mman.h>
+
+#define MAP_FLAG MAP_32BIT
+
+int main(int argc, char *argv[])
+{
+        void *p;
+
+        p = mmap(NULL, 4096, PROT_READ, MAP_SHARED | MAP_ANONYMOUS | MAP_FLAG, -1, 0);
+        if (p == MAP_FAILED)
+                perror("mmap");
+
+        printf("%d: p: %p\n", getpid(), p);
+
+        if (p)
+                munmap(p, 4096);
+
+        return 0;
+}
+__EOF
+
+make ${TEST_NAME}
+
+cat > ${TEST_DIR}/secadm.rules<<__EOF
+{
+        "applications": [
+                {
+                        path: "${TEST_DIR}/${TEST_NAME}",
+                        features: {
+				"disallow_map32bit" : true,
+                        }
+                }
+	]
+}
+__EOF
+
+sysctl hardening.pax.disallow_map32bit.status=2
+secadm flush
+secadm -c ${TEST_DIR}/secadm.rules set
+repeat 6 ./${TEST_NAME}
+
+# restore system policy
+secadm flush
+secadm set
