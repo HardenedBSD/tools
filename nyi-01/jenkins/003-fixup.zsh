ef0f317c2ae29b2dbf2dc884ed38aef272007ecd
Oliver Pinter <oliver.pinter@hardenedbsd.org>
2016-09-25 18:49:45 +0200

    HBSD: wipe out nyi-01 related scripts from tools repo.
    They now live under https://github.com/HardenedBSD/nyi01-tools repo.
    
    Signed-off-by: Oliver Pinter <oliver.pinter@hardenedbsd.org>

--- nyi-01/jenkins/003-fixup.zsh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env zsh
-
-_TAR_DIR="/jenkins/releases/${JOB_NAME}/build-${BUILD_NUMBER}/"
-
-# fixup for CHECKSUMs, to not contain full path of files
-for _file in $(find ${_TAR_DIR} -maxdepth 2 -name 'CHECKSUMS.SHA256' -o -name 'CHECKSUMS.SHA512' -o -name 'CHECKSUMS.MD5'); do
-	sed -i '' -e '/SHA256/s|^\(.*(\).*/\(.*\)|\1\2|g' -e '/SHA512/s|^\(.*(\).*/\(.*\)|\1\2|g' -e '/MD5/s|^\(.*(\).*/\(.*\)|\1\2|g' ${_file}
-done
-
